55783845ac76a496768ba58372f160d8c12033d3
romain.signes@gmail.com <romain.signes@gmail.com>
2015-03-16 11:28:22 +0100

    Comments added to the main file

--- js-engine.js
+++ js-engine.js
@@ -1,7 +1,10 @@
+// Require all the needed node modules
 var express = require('express');
 
+// Create an express object
 var app = express();
 
+// Get the URL path
 app.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/plain');
     res.end('It works !');
@@ -12,4 +15,5 @@ app.use(function(req, res, next){
     res.send(404, 'ERROR 404 - NOT FOUND');
 });
 
+// Listen to the specified port
 app.listen(8000);
